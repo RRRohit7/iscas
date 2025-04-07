e12a9a29797047cacef70c8a32a1077cfe68321a
Rohit <rohit7rrr@gmail.com>
2025-04-07 15:46:40 -0500

    Adding parser: Ignoring comments & removing spaces

--- project.cpp
+++ project.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
+#include<fstream>
+#include<string>
+#include<algorithm>
 
-int main(){
-	std::cout << "Hello World\n" << std::endl;
+using namespace std;
+
+string trim(string str){
+	 str.erase(remove(str.begin(), str.end(), ' '), str.end());
+	 return str;
+}
+void parser(string filename){
+	ifstream file(filename);
+	string line;
+	while(getline(file, line)){
+		if(line.empty() || line[0] == '#')
+			continue;
+		cout << trim(line) <<endl;
+	}
+}
+int main(int argc, char* argv[]){
+	parser(argv[1]);
 	return 0;
 }
