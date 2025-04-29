c21446dd87423623b074b55805493086a78659f9
Rohit <rohit7rrr@gmail.com>
2025-04-29 16:08:14 -0500

    Adding printing path logic

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: iscas
 
 iscas: project.cpp
-	g++ -g -o iscas project.cpp
+	g++ -std=c++17 -g -o iscas project.cpp
 
 clean:
 	rm -f iscas
--- project.cpp
+++ project.cpp
@@ -78,31 +78,82 @@ unordered_map<string, vector<pair<string, int>>> parser(string filename){
 	return adjList;
 }
 
+int distance_between_nodes(string node1, string node2, unordered_map<string, vector<pair<string, int>>>& adjList){
+	for(auto& [node, dist] : adjList[node1]){
+		if(node == node2)
+			return dist;
+	}
+	return INT_MAX;
+}
+
 int dijkstra_shortest_path(string input, string output, unordered_map<string, vector<pair<string, int>>>& adjList){
 	priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> prior_q;
-	unordered_map<string, int> distance;
+	unordered_map<string, pair<int, string>> distance;
 	for(auto& [node, vector] : adjList) {
-		distance[node] = INT_MAX;
+		distance[node].first = INT_MAX;
 	}
-	distance[input] = 0;
+	distance[input].first = 0;
+	distance[input].second = input;
 	prior_q.push({0, input});
 	while(!prior_q.empty()){
 		string min_node = prior_q.top().second;
 		prior_q.pop();
 		for(auto& [neighbour, weight] : adjList[min_node]){
-			if(distance[neighbour] > distance[min_node] + weight){
-				distance[neighbour] = distance[min_node] + weight;
-				prior_q.push({distance[neighbour], neighbour});
+			if(distance[neighbour].first > distance[min_node].first + weight){
+				distance[neighbour].first = distance[min_node].first + weight;
+				distance[neighbour].second = min_node;
+				prior_q.push({distance[neighbour].first, neighbour});
 			}
 		}
 	}
-	return distance[output];
+	string temp = output;
+	while(distance[temp].first != INT_MAX && temp != input){
+		string nextnode = distance[temp].second;
+		int dist_nextnode = distance_between_nodes(nextnode, temp, adjList);
+		cout << temp << " --" << dist_nextnode << "--> ";
+		if(nextnode == input){
+			cout << nextnode << endl;
+			break;
+		}
+		else { temp = nextnode; }
+	}
+	return distance[output].first;
 }
+
 
 int main(int argc, char* argv[]){
 	unordered_map<string, vector<pair<string, int>>> adjList;
+	if(argc != 4){
+		cout << "Incorrect number of arguments." << endl;
+		return 0;
+	} 
 	adjList = parser(argv[1]);
+	bool invalid_input = find(inputs.begin(), inputs.end(), argv[2]) == inputs.end() ? true : false;
+	bool invalid_output = find(outputs.begin(), outputs.end(), argv[3]) == outputs.end() ? true : false;
+	if(invalid_input || invalid_output){
+		if(invalid_input){
+			if(find(outputs.begin(), outputs.end(), argv[2]) == outputs.end()){
+				cout << "Signal " << argv[2] << " not found in " << argv[1] << endl;
+			}
+			else{
+				cout << "Signal " << argv[2] << " is not an input pin" << endl;
+			}
+		}
+		if(invalid_output){
+			if(find(inputs.begin(), inputs.end(), argv[3]) == inputs.end()){
+				cout << "Signal " << argv[3] << " not found in " << argv[1] << endl;
+			}
+			else{
+				cout << "Signal " << argv[3] << " is not an output pin" << endl;
+			}
+		}
+	return 0;
+	}
 	int short_path = dijkstra_shortest_path(argv[3], argv[2], adjList);
-	cout << "Shortest path: " << short_path << endl;
+	if(short_path != INT_MAX)
+		cout << "Path Length: " << short_path << endl;
+	else
+		cout << "No path found from " << argv[2] << " to " << argv[3] <<  endl;
+
 	return 0;
 }
